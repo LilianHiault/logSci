2f6abc3d58de8cd8524c2a3fc5b6a7db59ee3a20
Lilian HIAULT <lilian.hiault@etu.uca.fr>
2019-10-03 21:17:10 +0200

    Remplacé: TP3.pdf Modifié: tp3ex1.py

--- tp3/tp3ex1.py
+++ tp3/tp3ex1.py
@@ -2,6 +2,42 @@ import numpy as np
 
 
 # Functions
+# Question 1
+def code_une(lt):
+    return ord(lt) - 65
+
+
+# Question 2
+def decode_une(c):
+    return chr(c + 65)
+
+
+# Question 3
+def code_mot(m):
+    code = []
+    for i in m:
+        code.append(code_une(i))
+    return code
+
+
+def decode_mot(L):
+    for i in L:
+
+    return 0
 
 
 # Main
+# Question 1
+mot = input("Entrez un mot à coder : ")
+"""print("Code : ")
+for i in mot:
+    print(code_une(i))
+
+# Question 2
+code = eval(input("Code : "))
+print("Lettre correspondant à", code, ":", decode_une(code))"""
+
+# Question 3
+#motCode = input("Mot codé : ")
+print("Mot", mot, "codé :", code_mot(mot))
+#print("Mot ", motCode, "décodé :", decode_mot(motCode))
