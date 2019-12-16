4d7cc9c3a418275186ccdbde2da07d3c276755d4
Lilian Hiault <lilian.hiault@laposte.net>
2019-12-16 17:01:27 +0100

    TP5 exercice 7 fini

--- tp5/tp5ex7.py
+++ tp5/tp5ex7.py
@@ -6,3 +6,16 @@ from sympy import pprint, var
 var('x a n')
 f = (x**(n - 1)) / (a + x)
 vn = sm.integrate(f, (x, 0, 1))
+pprint(vn)
+
+print("")
+v40 = vn.subs(n, 40)
+pprint(v40)
+
+print("")
+v40 = v40.subs(a, 3)
+pprint(v40)
+
+print("")
+v40 = v40.evalf()
+pprint(v40)
