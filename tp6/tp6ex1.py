fad21f6879e235eba0266bea0db21f0eb5f4354d
Lilian Hiault <lilian.hiault@laposte.net>
2019-12-16 18:27:07 +0100

    TP 6 exercices 1 et 2

--- tp6/tp6ex1.py
+++ tp6/tp6ex1.py
@@ -13,6 +13,7 @@ def indiceMin(liste):
         comp += 1
     return iMin
 
+
 def totalAcc(liste):
     sum = 0
     for elem in liste:
@@ -26,19 +27,27 @@ cc = []  # Cours à la clôture
 cma = []  # Cours maximal du jour
 cmi = []  # Cours minimal du jour
 dt = []  # Date du jour
-acc = [] # Accroissements journaliers
+acc = []  # Accroissements journaliers
+accpos = 0  # Accroissements journaliers positifs
+accneg = 0  # Accroissements journaliers négatifs
+accroi = 0
 
 # Ouverture et lecture
 with open("cac40.csv", encoding="utf-8") as fichier:
     for ligne in fichier:
         # print(ligne)
-        ligneEnCours = ligne.split(";")
-        co.append(float(ligneEnCours[2]))
-        cc.append(float(ligneEnCours[5]))
-        cma.append(float(ligneEnCours[3]))
-        cmi.append(float(ligneEnCours[4]))
-        dt.append(ligneEnCours[1])
-        acc.append(float(ligneEnCours[2]) - float(ligneEnCours[5]))
+        ligneCour = ligne.split(";")
+        co.append(float(ligneCour[2]))
+        cc.append(float(ligneCour[5]))
+        cma.append(float(ligneCour[3]))
+        cmi.append(float(ligneCour[4]))
+        dt.append(ligneCour[1])
+        accroi = float(ligneCour[2]) - float(ligneCour[5])
+        acc.append(accroi)
+        if accroi >= 0:
+            accpos += accroi
+        else:
+            accneg -= accroi
 
 # Nombre de jours
 print(len(dt), "jours sont répertoriés dans le fichier.")
@@ -49,4 +58,11 @@ print("Le cours le plus bas dans la période est",
       cma[iCmaPlusBas], "le", dt[iCmaPlusBas])
 
 # Accroissements journaliers
-print("Total accroissements journaliers :",totalAcc(acc))
+print("Total accroissements journaliers :", totalAcc(acc))
+
+# plt.plot(acc)
+plt.bar(0.5, accpos, 1, color='r', label='Accroissement positif', alpha=0.5)
+plt.bar(2.5, accneg, 1, color='b', label='Accroissement négatif', alpha=0.5)
+plt.title('Accroissement total')
+plt.legend(loc=0)
+plt.show()
--- tp6/tp6ex2.py
+++ tp6/tp6ex2.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Fonctions
@@ -20,11 +21,12 @@ def medLi(liste):
     if len(liste) % 2 == 0:
         return (listeMed[len(liste) / 2] + listeMed[(len(liste) / 2) + 1]) / 2
     else:
-        return listeMed[len(liste)//2]
+        return listeMed[len(liste) // 2]
+
 
 def popMoy(liste):
     lPopMoy = []
-    nbVilles = 0;
+    nbVilles = 0
     for popVille in liste:
         if popVille >= 1000 and popVille <= 20000:
             lPopMoy.append(popVille)
@@ -32,12 +34,30 @@ def popMoy(liste):
 
 
 def distPts(xa, ya, xb, yb):
-    return ((xb - xa)**2 + (yb - ya)**2)**(1/2)
+    return ((xb - xa)**2 + (yb - ya)**2)**(1 / 2)
 
+
+def agglomeration(ville):
+    bool = 1
+    i = 0
+    longitude = 0
+    latitude = 0
+    while bool and i < len(nom):
+        if nom[i] == ville:
+            longitude = long[i]
+            latitude = lat[i]
+            bool = 0
+    print("longitude", longitude, "| latitude", latitude)
+    proche = 20 * (1.56961 * 10**(-4))
+    nbVilles = 0
+    for i in range(len(nom)):
+        distance = distPts(longitude, latitude, long[i], lat[i])
+        if distance <= proche:
+            nbVilles += 1
+    print(nbVilles, "sont proches de", ville)
 
 
 # Main
-
 # Listes
 insee = []
 nom = []
@@ -64,41 +84,67 @@ with open("villes.csv", encoding="utf-8") as fichier:
 
 # Analyse des résultats
 
-# Ville la plus au Nord
-print(nom[iMaxL(lat)], "est la ville la plus au Nord.")
+# # Ville la plus au Nord
+# print(nom[iMaxL(lat)], "est la ville la plus au Nord.")
+#
+# # Latitude et longitude médiane
+# print("La latitude médiane est :", medLi(lat))
+# print("La longitude médiane est :", medLi(long))
+#
+# # Commune médiane
+# distMed = distPts(lat[0], medLi(lat), long[0], medLi(long))
+# indice = 0
+# for i in range(len(nom)):
+#     if distPts(lat[i], medLi(lat), long[i], medLi(long)) < distMed:
+#         distMed = distPts(lat[i], medLi(lat), long[i], medLi(long))
+#         indice = i
+#
+# print("La ville la plus proche du point médian est ", nom[indice], "dont les coordonnées sont : ", long[indice], lat[indice])
+#
+# # Population des communes ayant entre 1000 et 20 000 habitants
+# popFaible = []
+# villesPopFaible = []
+#
+# comp = 0
+# for i in range(len(nom)):
+#     if pop[i] >= 1000 and pop[i] <= 20000:
+#         popFaible.append(pop[i])
+#         villesPopFaible.append(nom[i])
+#         comp += 1
+#
+# index = np.arange(comp)
+# largeur = 0.5
+#
+# plt.figure("Villes avec entre 1000 et 20 000 habitants.")
+# plt.bar(index, popFaible, largeur, color='r',
+#         alpha=0.5, label='Population totale')
+# plt.xticks(index, villesPopFaible, rotation=90)
+#
+#
+# # Visualiser les communes de France
+# print("Nom des villes de plus de 100 000 habitants :")
+# latGr = []
+# longGr = []
+# latPet = []
+# longPet = []
+# for i in range(len(nom)):
+#     if pop[i] > 100000:
+#         latGr.append(lat[i])
+#         longGr.append(long[i])
+#     else:
+#         latPet.append(lat[i])
+#         longPet.append(long[i])
+#         print(nom[i])
+#
+# plt.figure("Communes de France")
+# plt.plot(longPet, latPet, '.', color='b', markersize=0.2, alpha=0.5,
+#          label='Villes de moins de 100 000 habitants.')
+# plt.title("Communes de France")
+# plt.plot(longGr, latGr, '.', color='r', markersize=5, alpha=0.9,
+#          label='Villes de plus de 100 000 habitants.')
+# plt.axis("equal")
+# plt.legend(loc=0)
+#
+# plt.show()
 
-# Latitude et longitude médiane
-print("La latitude médiane est :", medLi(lat))
-print("La longitude médiane est :", medLi(long))
-
-# Commune médiane
-distMed = distPts(lat[0], medLi(lat), long[0], medLi(long))
-indice = 0
-for i in range(len(nom)):
-    if distPts(lat[i], medLi(lat), long[i], medLi(long)) < distMed:
-        distMed = distPts(lat[i], medLi(lat), long[i], medLi(long))
-        indice = i
-
-print("La ville la plus proche du point médian est ", nom[indice], "dont les coordonnées sont : ", long[indice], lat[indice])
-
-# Population des communes ayant entre 1000 et 20 000 habitants
-
-# Visualiser les communes de France
-latGr = []
-longGr = []
-latPet = []
-longPet = []
-for i in range(len(nom)):
-    if pop[i] > 100000:
-        latGr.append(lat[i])
-        longGr.append(long[i])
-    else:
-        latPet.append(lat[i])
-        longPet.append(long[i])
-
-plt.plot(longPet, latPet, '.', color = 'b')
-plt.plot(longGr, latGr, '.', color = 'r', markersize = 5)
-plt.legend(loc=0)
-plt.title("Communes de France")
-plt.axis("equal")
-plt.show()
+agglomeration("Clermont-Ferrand")
